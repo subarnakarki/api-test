7bd2da0bc64e9ca1571bd8bc6c78893ed4d2009a
Subarna Karki <subarna.karki13@gmail.com>
2020-10-23 13:32:37 -0400

    add tests for listing breweries

---
+++ test/list-breweries.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const got = require('got');
+const endpoint = "https://api.openbrewerydb.org/breweries";
+
+
+test('searching /breweries should return results', async () => {
+  const response = await got(`${endpoint}`);
+  const data = JSON.parse(response.body);
+  expect(data.length).toBeGreaterThan(0);
+});
+
+test('searching /breweries by_city=san_diego should return 20 results', async() => {
+  const response = await got(`${endpoint}?by_city=san_diego`);
+  const data = JSON.parse(response.body);
+  expect(data.length).toBe(20);
+});
+
+test('searching /breweries by_name=dog should only return results with a name containing "dog"', async() => {
+  const response = await got(`${endpoint}?by_name=dog`);
+  const data = JSON.parse(response.body);
+  data.forEach(result => {
+    expect(result.name.toLowerCase()).toContain("dog");
+  });
+});
